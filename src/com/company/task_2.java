3f9200ba0914dd98932d777f2443d788ba3e3cbb
disakov <disakov@phoenixit.ru>
2020-07-23 21:32:42 +0300

    task 2 commit

--- src/com/company/task_2.java
+++ src/com/company/task_2.java
@@ -4,10 +4,11 @@ public class task_2 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
+        for (int i = 2; i < 10; i++) {
+            for (int j = 2; j < 10; j++) {
                 System.out.printf("%d * %d = %d \n", i, j, i * j);
             }
+            System.out.println("\n");
         }
     }
 }
