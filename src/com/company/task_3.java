a43989a36de3e39a82c0926e927bebf67dce091b
disakov <disakov@phoenixit.ru>
2020-07-25 19:45:03 +0300

    task 3 commit

--- src/com/company/task_3.java
+++ src/com/company/task_3.java
@@ -6,7 +6,7 @@ public class task_3 {
 
 //1. Вывести массив в консоль.
         System.out.println("Пункт 1");
-        int [] array = {-23, 56, 1, -421, 5, 100, 34, 95, -153, -953, 100, 321, 932, 0, 932, -1, 9, -11, 99, -101, 15, 328, 481, 424, -92, -124, 24, 7, 78, 921};
+        int [] array = {-23, 56, 1, -421, 5, 100, 34, 95, -153, -953, 100, 321, 932, 0, 932, -1, 9, -11, 99, -101, 15, 328, 481, 424, -92, -124, 24, 7, 78, 988};
         for (int i = 0; i < array.length; i++) {
             System.out.printf("Элемент массива %d: %d \n", i + 1, array[i]);
         }
@@ -15,7 +15,7 @@ public class task_3 {
 //2. Вывести в консоль факториалы всех однозначных чисел больше 0 из исходного массива.
         System.out.println("\n\nПункт 2");
         for (int i = 0; i < array.length; i++) {
-            if (array[i] >= 0  && array[i] <= 10) {
+            if (array[i] > 0  && array[i] < 10) {
                 int result = 1;
                 for (int j = 1; j <= array[i]; j++) {
                     result = result * j;
@@ -28,7 +28,7 @@ public class task_3 {
 //3. Вывести в консоль все двузначные числа перед этим разделив на 2 все четные и умножив на 3 все нечетные среди них.
         System.out.println("\n\nПункт 3");
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 9 && array[i] < 100) {
+            if ((array[i] > 9 && array[i] < 100) || (array[i] < -9 && array[i] > -100)) {
                 if (array[i] % 2 == 0) {
                     System.out.printf("Четный элемент: %d; итог: %d \n", array[i], array[i] / 2);
                 } else {
@@ -42,7 +42,7 @@ public class task_3 {
         System.out.println("\n\nПункт 4");
         int max = array[1];
         for(int i = 1; i < array.length; i+=2) {
-            if (array[i] <= -100 || array[i] >=100) {
+            if ((array[i] <= -100 && array[i] > -1000) || (array[i] >=100 && array[i] < 1000)) {
                 if (array[i] > max)
                     max = array[i];
             }
@@ -54,7 +54,7 @@ public class task_3 {
         System.out.println("\n\nПункт 5");
         int min = array[0];
         for(int i = 0; i < array.length; i+=2) {
-            if (array[i] <= -100 || array[i] >=100) {
+            if ((array[i] <= -100 && array[i] > -1000) || (array[i] >=100 && array[i] < 1000)) {
                 if (array[i] < min)
                     min = array[i];
             }
